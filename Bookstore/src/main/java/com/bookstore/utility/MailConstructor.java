b78351f05628b7b0ae0a544fe80acb0eb4deb7c6
Ye Wu <borderwing@sjtu.edu.cn>
2017-06-14 21:38:55 +0800

    version 1.0 email varification completed

---
+++ Bookstore/src/main/java/com/bookstore/utility/MailConstructor.java
@@ -0,0 +1,31 @@
+package com.bookstore.utility;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
+
+import com.bookstore.domain.User;
+
+@Component
+public class MailConstructor {
+	
+	@Autowired
+	private Environment env;
+	
+	public SimpleMailMessage constructResetTokenEmail(
+			String contextPath, Locale locale, String token, User user, String password
+			){
+		String url = contextPath + "/newUser?token="+token;
+		String message = "\n Please click on this link to verify email and edit info. Your password is: \n"+password;
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setTo(user.getEmail());
+		email.setSubject("New User");
+		email.setText(url+message);
+		email.setFrom(env.getProperty("support.email"));
+		return email;
+		
+	}
+}
